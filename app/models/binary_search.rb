b2bb81cbdda746cd9c6883a48c55502f09c66e85
rregalado17 <rregalado17@gmail.com>
2023-01-16 00:33:48 -0500

    binary search algo

---
+++ app/models/binary_search.rb
@@ -0,0 +1,24 @@
+class Binary_Search < ActiveRecord::Base
+
+    def binary_search(array, key)
+        low, high = 0, array.length - 1
+        while low <= high
+          mid = (low + high) >> 1
+          case key <=> array[mid]
+            when 1
+              low = mid + 1
+            when -1
+              high = mid - 1
+            else
+              return mid
+          end
+        end
+    end
+
+    debug
+
+    arr = [1,3,4,12,16,21,34,45,55,76,99,101]
+    key = 101
+    p binary_search(arr, key)
+
+end
