f1fde9289a6894760ef8029240f750ec1dc7025c
rregalado17 <rregalado17@gmail.com>
2023-01-15 22:50:18 -0500

    draft linear search algo

---
+++ app/models/linear_search.rb
@@ -0,0 +1,15 @@
+class Linear_Search < ActiveRecord::Base
+
+    def linear_search(array, key)
+        i = 0
+        
+        while i < array.length
+            if array[i] == key
+                return "#{key} at index #{array.index(key)}"
+            end
+                i+=1
+            end
+        return -1
+    end
+
+end
